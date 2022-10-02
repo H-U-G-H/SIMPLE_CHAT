563bce4cc056791ffc0b7b07666a70685aee2794
Исмаилов Саид <mazgOFF.net@mail.ru>
2022-10-02 21:35:49 +0300

    feat: добавлена возможность одновременно и получать и отправлять сообщения.

--- SimpleChatClientA.java
+++ SimpleChatClientA.java
@@ -6,22 +6,43 @@ import java.awt.event.*;
 
 public class SimpleChatClientA
 {
-    JTextField outgoing; // рейярнбне онке вюрю
+    JTextArea incoming; // онке бшбндю
+    JTextField outgoing; // онке ббндю
+    BufferedReader reader; // бундмни онрнй
     PrintWriter writer; // бшундмни онрнй
     Socket socket; // янйер
+
+    public static void main(String[] args) // нямнбмни лернд
+    {
+        new SimpleChatClientA().go(); // гюосяй йкхемрю
+    }
 
     public void go()
     {
         JFrame frame = new JFrame("оПНЯРНИ ВЮР-ЙКХЕМР"); // нямнбмне нймн
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // гюбепьемхе опнцпюллш опх гюйпшрхх нймю
         JPanel mainPanel = new JPanel(); // нямнбмюъ оюмекэ
+
+        incoming = new JTextArea(15, 50); // онке бшбндю
+        incoming.setLineWrap(true); // оепемня ярпнй
+        incoming.setWrapStyleWord(true); // оепемня он якнбюл
+        incoming.setEditable(false); // гюопер мю педюйрхпнбюмхе
+        JScrollPane scroller = new JScrollPane(incoming); // яйпнккеп дкъ онкъ бшбндю
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); // бепрхйюкэмши яйпнккхмц
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); // цнпхгнмрюкэмши яйпнккхмц
+
         outgoing = new JTextField(20); // хмхжхюкхгюжхъ рейярнбнцн онкъ вюрю
         JButton sendButton = new JButton("нРОПЮБХРЭ"); // ймнойю дкъ нропюбкемхъ яннаыемхи б вюр
         sendButton.addActionListener(new SendButtonListener()); // днаюбкемхе яксьюрекъ дкъ ймнойх
+        mainPanel.add(scroller); // днаюбхрэ яйпнккеп мю оюмекэ
         mainPanel.add(outgoing); // днаюбхрэ рейярнбне онке вюрю мю цкюбмсч оюмекэ
         mainPanel.add(sendButton); // днаюбхрэ ймнойс мю цкюбмсч оюмекэ
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel); // днаюбкемхе оюмекх б жемрп нймю
         setUpNetworking(); // лернд мюярпнийх яерх
+
+        Thread readerThread = new Thread(new IncomingReader()); // мнбши онрнй дкъ времхъ
+        readerThread.start(); // гюосяй онрнйю
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel); // днаюбкемхе оюмекх б жемрп нймю
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // гюбепьемхе опнцпюллш опх гюйпшрхх нймю
         frame.setSize(400, 500); // пюглеп нямнбмнцн нймю
         frame.setVisible(true); // бшбнд нямнбмнцн нймю мю щйпюм
     }
@@ -31,6 +52,8 @@ public class SimpleChatClientA
         try
         {
             socket = new Socket("127.0.0.1", 5000); // сярюмнбйх янйерю
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream()); // онрнй янедхмемхъ
+            reader = new BufferedReader(streamReader); // жеомни онрнй
             writer = new PrintWriter(socket.getOutputStream()); // янедхмемхе онрнйнб
             System.out.println("яЕРЭ ЯНГДЮМЮ"); // бшбнд яннаыемхе на сдювмнл хяунде
         }
@@ -60,9 +83,22 @@ public class SimpleChatClientA
         }
     } // OUT OF INNER CLASS
 
-    public static void main(String[] args) // нямнбмни лернд
+    public class IncomingReader implements Runnable // гюдювю
     {
-        new SimpleChatClientA().go(); // гюосяй йкхемрю
+        public void run() // едхмярбеммши лернд хмрептеияю RUNNABLE
+        {
+            String message; // бпелеммюъ оепелеммюъ дкъ упюмемхъ бундъыху яннаыемхи
+
+            try
+            {
+                while ((message = reader.readLine()) != null) // опх мюкхвхх ярпнйх, днаюбхрэ е╗ б MESSAGE
+                {
+                    System.out.println("времхе: " + message); // йнмянкэмши бшбнд дкъ деаюцхмцю
+                    incoming.append(message + "\n"); // APPEND() - днаюбкъер рейяр б сфе хлечыхияъ с онкъ TEXTAREA
+                }
+            }
+            catch(Exception exception) {exception.printStackTrace();} // напюанрйю хяйкчвемхи
+        }
     }
 }
 
